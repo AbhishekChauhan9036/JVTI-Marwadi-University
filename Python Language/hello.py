8f21a9f7023ba3df12bfe73179520fb212a06fde
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-07-30 09:42:18 +0530

    Bsc Fintech Sem 1

---
+++ Python Language/hello.py
@@ -0,0 +1,39 @@
+a = 10     # First variable
+b = 20     # Second Variable
+c = 10.87
+stri ="Rohan"
+fname = "Mohan"
+lname = "Rajput"
+
+"""print(type(c))
+print(type(b))
+print(type(stri))
+"""
+
+"""
+multiple 
+Line
+Comments
+"""
+"""
+if a>b:
+    print("a is greater than b")
+else:
+    print("a is less than b")
+"""
+
+
+x, y, z = 10, 20, 30
+# print(x,y,z)
+
+
+# function in python
+"""
+def add(a,b):
+    return a+b
+
+print(add(1,8))
+"""
+
+
+
