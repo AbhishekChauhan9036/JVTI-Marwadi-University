b2cf62465e10d926a4d65233aec835a2c6160f36
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-08-07 11:06:34 +0530

    variable and data type

--- C Language/main.c
+++ C Language/main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    printf("Hello World!");
+    //printf("\tHello World!\n");
+    //printf("\tIndia\n");
+    /*
+        this
+        is
+        multiline
+        comment
+    */
+
+    int salary = -10001;
+    float prize = -12.90;
+    double pi = 3.1416;
+    char ch = 'A';   //this is char value
+    printf("Salary is: %d\n",salary);
+    printf("Prize is: %f\n",prize);
+    printf("Value of Pi is: %lf\n",pi);
+    printf("Value of Ch is: %c\n",ch);
+
     return 0;
 }
