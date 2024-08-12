5d413f3a13524cf35e0bf0bab3ce7f00749d0f60
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-08-12 09:30:15 +0530

    Case Study and Assignment Sumbittion 1

---
+++ Python Language/caseStudyOne.py
@@ -0,0 +1,87 @@
+'''Task 1: Assign an integer value to a variable and print it.'''
+
+salary = 40000
+print(salary)
+
+
+
+'''Task 2: Assign a floating-point number to a variable and print it.'''
+
+pi = 3.1416
+print(pi)
+
+
+
+'''Task 3: Assign a string to a variable and print it.'''
+
+Name = "Abhishek"
+print(Name)
+
+
+
+'''Task 4: Assign a boolean value to a variable and print it.'''
+
+istaskCompleted = True
+print(istaskCompleted)
+
+
+
+'''Task 5: Assign a None value to a variable and print it.'''
+
+unsignedValue = None
+print(unsignedValue)
+
+
+
+'''Task 6: Assign your first name to a variable and your last name to 
+another variable. Concatenate them to form your full name and print it.'''
+
+fName ="Abhishek"
+lName = "Chauhan"
+fullName = fName+" "+lName
+print(fullName)
+
+
+
+'''Task 7: Assign your favorite quote to a variable. Concatenate it with 
+the author's name, separated by a hyphen, and print the complete quote.'''
+
+quote = "Success is not final, failure is not fatal: It is the courage to continue that counts"
+AuthorName = " --Winston S. Churchill"
+print(quote,AuthorName)
+
+
+
+'''Task 8: Create three variables representing your hometown, current city,
+ and dream destination. Concatenate these variables into a single sentence
+  like: "I was born in [Hometown], currently live in [Current City], and dream 
+  of visiting [Dream Destination]." Print the sentence.'''
+
+hometown = "Awarai Kala"
+currentCity = "Rajkot"
+dreamDestination = "Silicon Valley"
+print("I was born in", hometown + ",", "currently live in", currentCity + ",", "and dream of visiting", dreamDestination + ".")
+
+
+
+'''Task 9: Assign a string of your favorite food and another string of your
+ favorite drink. Concatenate them with the word "and" in between, and print the result.'''
+
+favorite_food = "Pizza"
+favorite_drink = "Iced Tea"
+result = favorite_food + " and " + favorite_drink
+print(result)
+
+
+
+
+
+'''Task 10: Assign two strings representing the first part and the second part of a
+ famous saying (e.g., "A journey of a thousand miles" and "begins with a single step"). 
+ Concatenate them with a comma and a space in between, and print the complete saying.'''
+
+first_part = "A journey of a thousand miles"
+second_part = "begins with a single step"
+complete_saying = first_part + ", " + second_part
+print(complete_saying)
+
