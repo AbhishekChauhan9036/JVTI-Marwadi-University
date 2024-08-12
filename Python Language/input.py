3e75ac089bca0644cd2358c8ee61438981c31305
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-08-12 10:15:36 +0530

    how to take user input in python

---
+++ Python Language/input.py
@@ -0,0 +1,20 @@
+''' How to take user Input a String Value'''
+name = input("Enter Your Name: ")
+print(name)
+
+
+'''How to take user Input a Integer Value'''
+salary = int(input("Enter your exact Salary:"))
+print(salary)
+print(type(salary))
+
+
+'''How to take user Input a Float Value'''
+pi = float(input("Enter the value of pi: "))
+print("Value of pi is: ",pi)
+
+
+'''How to take user input a Boolean value'''
+isTrue = (input("Enter a value of is isTrue: "))
+bool_value = isTrue.lower() == 'true'
+print(bool_value)
