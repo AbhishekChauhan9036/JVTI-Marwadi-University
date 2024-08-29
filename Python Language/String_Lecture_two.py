d3c6d476ef95fac6cb29f62bdb2792acedff8aaa
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-08-29 10:05:02 +0530

    Marwadi University

---
+++ Python Language/String_Lecture_two.py
@@ -0,0 +1,22 @@
+# https://www.w3schools.com/python/python_strings_escape.asp
+# roll_Number = 12
+# my_Name = "Abhishek Chauhan"
+# print("My Role Number is:",roll_Number, "and My Name is:",my_Name)
+
+# print(f"My Role Number is: {roll_Number} and My Name is: {my_Name}")
+
+
+# price = 59
+# print(f"The price is {price:.4f} Rs")
+
+
+
+# txt = f"The price is {20 + 59} Rs"
+# print(txt)
+
+
+# print("Abhishek ",end='')
+# print("Chauhan")
+
+
+
--- Python Language/text.py
+++ Python Language/text.py
@@ -1,7 +1,8 @@
-input_year = int(input("Enter a year: "))
-if (input_year % 4 == 0 and input_year % 100 != 0) or (input_year % 400 == 0):
-    print(f"{input_year} is a Leap Year")
-else:
-    print(f"{input_year} is not a Leap Year")
+s = "  Abhishek Chauhan"
+print(s[0])
+print(s[2:])
+print(s.strip())
 
 
+a = 10,20,30
+print(a)
