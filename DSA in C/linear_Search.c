fa0d961204ba84952eeb80257e916f0098672ed7
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-09-04 08:39:13 +0530

    MU

--- DSA in C/linear_Search.c
+++ DSA in C/linear_Search.c
@@ -1,19 +1,22 @@
-#include<stdio.h>
-int main(){
-    int arr[] ={12,17,19,18,31,45,67,78};
-    int n=8;
-    int target = 78;
-    for(int i=0;i<n;i++){
-        if(arr[i]==target){
-            return i;
-        }
-        return -1;
-    }
+#include <stdio.h>
 
+int main()
+{
+    int arr[] = {12, 17, 19, 18, 31, 45, 67, 78};
+    int n = 8;
+    int target = 78;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            printf("%d", i);
+            return 0;
+        }
+    }
+    printf("-1");
+    
     return 0;
 }
-
-
-
 
 
