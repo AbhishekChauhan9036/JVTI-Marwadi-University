4d566229468fbe0f1a846ed72bafee18dfc21659
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-10-14 18:17:02 +0530

    MU

---
+++ C Language/Arrays/Arrays1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int marks[7] = {10, 20, 30, 40, 50, 60, 70};
+    for (int i = 0; i <= 6; i++)
+    {
+        printf("%d ", marks[i]);
+    }
+    return 0;
+}
