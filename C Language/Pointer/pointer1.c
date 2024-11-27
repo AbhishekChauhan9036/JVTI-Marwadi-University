074daf165fe8234a218647b252f33d814ab5acd3
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-11-27 11:27:10 +0530

    MU

---
+++ C Language/Pointer/pointer1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter a Number: ");
+    scanf("%d", &b);
+    printf("Enter a Number: ");
+    scanf("%d", &a);
+    sum = a + b;
+    printf("Sum of a and b is: %d", sum);
+    return 0;
+}
