2763980b04157174f9e9b9d733de3ac48b6ef50d
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-01-02 15:35:11 +0530

    MU

---
+++ Fintech User Interface II/script.js
@@ -0,0 +1,12 @@
+var fName = "Vimal";
+var lName = "Kanzariya"
+var num = 10;
+var float = 3.14;
+var isTrue = false;
+console.log(fName);
+console.log(lName)
+// alert("Hi Kya chal rha hai")
+const a = 10;
+const b = 20;
+var c = a + b;
+console.log(c);
