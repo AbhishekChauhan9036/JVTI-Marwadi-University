f37fb7a00ac53cb415f6dd2715f3873a6629133e
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-01-03 17:31:55 +0530

    MU

--- Fintech User Interface II/script.js
+++ Fintech User Interface II/script.js
@@ -1,3 +1,4 @@
+/**
 var fName = "Vimal";
 var lName = "Kanzariya"
 var num = 10;
@@ -10,3 +11,38 @@ const a = 10;
 const b = 20;
 var c = a + b;
 console.log(c);
+
+**/
+
+
+let x = 5 + 5;
+let y = +"5" + 5;
+let z = "Hello" + 5;
+
+const arr = [10, ["Hi", "Kya"], 89, "Nandani"];
+// console.log(typeof arr)
+
+
+const student = {
+    name: "Ayush",
+    rollNumber: 12345,
+    grNumber: 36377,
+    Address: "Rajkot",
+    IQLevel: 0,
+    Stream: "BSc IT Fintech"
+};
+
+// console.log(typeof student)
+// console.log(student)
+
+
+
+
+
+function sum(a, b) {
+    return a+b;
+}
+const sub = function(a,b){
+    return a-b;
+}
+console.log("sub of a and b is: ", sub(10,20));
