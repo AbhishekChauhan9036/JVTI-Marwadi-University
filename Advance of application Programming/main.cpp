e4bc35c3fefcf5d3a027d275113a361030b6d37b
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2025-01-07 15:08:49 +0530

    C++

---
+++ Advance of application Programming/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Cloud
+{
+public:
+    void printStudentName()
+    {
+        cout << "Ved" << endl;
+        cout << "Abhay" << endl;
+        cout << "Gautam" << endl;
+        cout << "Mann" << endl;
+        cout << "Arman" << endl;
+    }
+};
+
+int main()
+{
+    Cloud c1;
+    c1.printStudentName();
+    return 0;
+}
