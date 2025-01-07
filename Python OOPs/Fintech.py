f2fe1ac26c9f5c0c8f5ab6b718fafe7505c16604
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2025-01-07 12:19:46 +0530

    MU

---
+++ Python OOPs/Fintech.py
@@ -0,0 +1,13 @@
+class Fintech:  #Class
+
+    def __init__(self): # Constructor
+        pass
+
+    numberOfStudents = 2  #Variable or Attribute
+    def printStudentName(self): # Function
+        print("Sunil\nVivan")
+
+
+obj = Fintech()  # Object Creation
+obj.printStudentName() # Function Call using . operator
+
