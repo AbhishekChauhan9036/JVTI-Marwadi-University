5e291cd62f1d7ecf0d0868fc215f5159f5ca183e
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2025-01-07 10:43:56 +0530

    JS

--- Fintech User Interface II/script.js
+++ Fintech User Interface II/script.js
@@ -7,7 +7,7 @@ var isTrue = false;
 console.log(fName);
 console.log(lName)
 
-// alert("Hi Kya chal rha hai")
+
 
 const a = 10;
 const b = 20;
@@ -19,9 +19,15 @@ console.log(c);
 let x = 5 + 5;
 let y = +"5" + 5;
 let z = "Hello" + 5;
+// console.log(x)
+// console.log(y)
+// console.log(z)
 
-const arr = [10, ["Hi", "Kya"], 89, "Nandani"];
+
+
+// const arr = [10, ["Hi", "Kya"], 89, "Nandani"];
 // console.log(typeof arr)
+// console.log(arr);
 
 
 const student = {
@@ -30,20 +36,58 @@ const student = {
     grNumber: 36377,
     Address: "Rajkot",
     IQLevel: 0,
-    Stream: "BSc IT Fintech"
 };
 
+student.name = "Shubham"
+student.Stream = "BSc IT Fintech"
 // console.log(typeof student)
-// console.log(student)
+// console.log(student.name)
 
 
+// alert("Hi Kya chal rha hai")
 
 
-
-function sum(a, b) {
+function sum(a, b=10) {
     return a+b;
 }
+
+let ans = sum(10);
+// console.log(ans)
+
+
+
 const sub = function(a,b){
     return a-b;
 }
-console.log("sub of a and b is: ", sub(10,20));
+// console.log("sub of a and b is: ", sub(10,20));
+
+
+let StudentName = "Sunil Bajeja"                                   
+// console.log(StudentName.toLowerCase());
+// console.log(StudentName.toUpperCase());
+// console.log(StudentName.length)
+// console.log(StudentName.charAt(4))
+// console.log(StudentName[0])
+// console.log(StudentName.charCodeAt(1))
+// console.log(StudentName.at(2))
+// console.log(StudentName.slice(2,8))
+
+
+let str1 = "Takshay";
+let str2 = "Bhalodiya";
+let str = str1+" "+str2;
+let str4 = str1.concat(" "+str2);
+// console.log(str4);
+
+
+
+let text1 = "      Hello World!      ";
+let text2 = text1.trim()
+let text3 = text1.trimStart();
+let text4 = text1.trimEnd();
+
+
+
+
+
+
