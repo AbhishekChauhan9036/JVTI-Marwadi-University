81a8e172aa8bc19a368082975d3f25461d3113ad
tourist <tourist@kali.kali>
2025-01-15 11:47:40 +0530

    MU

---
+++ Advance of application Programming/Assignment1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Employee{
+    private:
+        String name;
+        int age;
+        String Designation;
+        double salary;
+    public:
+        Employee(){
+            
+        }
+}
+int main(){
+
+}
