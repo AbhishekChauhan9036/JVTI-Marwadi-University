2b3385b035e0deddbf0bc15f8cf0803d3657a65e
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-02-24 10:56:50 +0530

    MU

--- Fintech User Interface II/AdvanceJs/script.js
+++ Fintech User Interface II/AdvanceJs/script.js
@@ -1,9 +1,9 @@
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 // let ans = arr.filter((num) => num >= 3);
-let ans = arr.filter(function (num) {
-  return num >= 3;
-});
-console.table(ans);
+// let ans = arr.filter(function (num) {
+//   return num >= 3;
+// });
+// console.table(ans);
 
 /**
  * Create an Array of length 9 which contains numbers
@@ -11,6 +11,28 @@ console.table(ans);
  * number is Negative and store another variable and
  * print it.
  */
-const Array = [1, 2, -3, -4, -5, 6, 7, 8, 9];
-let NegativeNumber = Array.filter((num) => num < 0);
-console.table(NegativeNumber);
+// const Array = [1, 2, -3, -4, -5, 6, 7, 8, 9];
+// let NegativeNumber = Array.filter((num) => num < 0);
+// console.table(NegativeNumber);
+
+function reverseArray(arr) {
+  const ans = [];
+  for (let i = 0; i < arr.length; i++) {
+    ans.push(arr[arr.length - i - 1]);
+  }
+  return ans;
+}
+
+function maxElementArray(arr) {
+  let maxelement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxelement) {
+      maxelement = arr[i];
+    }
+  }
+  return maxelement;
+}
+
+const arr = [13, 90, 37, 56, 57];
+console.log(reverseArray(arr));
+console.log(maxElementArray(arr));
