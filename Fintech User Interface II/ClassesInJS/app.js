b898546e962313d0a236f3b1a176a338ae72bbd9
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-03-17 10:32:11 +0530

    MU

---
+++ Fintech User Interface II/ClassesInJS/app.js
@@ -0,0 +1,21 @@
+/*
+ OOps in JavaScript->
+    Variable -> Data Member
+    Function -> Member Function/ Method
+
+ pillars of OOPS->
+    Encapsulation
+    Data Abstraction
+    Inheritance
+    Polymorphism
+*/
+
+class Car{
+    constructor(n, m){
+        this.name = n;
+        this.model = m;
+    }
+}
+
+const car1 = new Car("KIA",2024);
+const car2 = new Car("PAGANI",2025);
