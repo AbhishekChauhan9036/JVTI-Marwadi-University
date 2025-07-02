3900efe7f29f7e8680cbe1d11d40608d5d7e3c67
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-07-02 10:21:58 +0530

    MU

---
+++ Fintech Programming Language II/Lecture1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1;
+    cout<<"Enter the value of num1: ";
+    cin>>num1;
+
+    int num2;
+    cout<<"Enter the value of num2: ";
+    cin>>num2;
+
+    int sum = num1 + num2;
+    cout<<"sum of num1 and num2 is: "<<sum<<endl;
+    return 0;
+}
+
+// :: Scope Resolution Operator
+// << Insertion Operator
+// >> Extraction Operator
