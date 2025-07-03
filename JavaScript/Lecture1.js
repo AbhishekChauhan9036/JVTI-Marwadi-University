7387db6dd66cee6e84011f7a7a06a5af53043573
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-07-03 10:39:44 +0530

    MU

---
+++ JavaScript/Lecture1.js
@@ -0,0 +1,16 @@
+console.log("Namaste JavaScript");
+
+var Name = "Kamil";
+console.log(Name, typeof Name);
+
+var num = 100;
+console.log(num, typeof num);
+
+var prize = 10.9;
+console.log(prize, typeof prize);
+
+var isTrue = false;
+console.log(isTrue, typeof isTrue);
+
+
+// fName, lName, Age, Enrollment_Num, Mob_num, ColleName
