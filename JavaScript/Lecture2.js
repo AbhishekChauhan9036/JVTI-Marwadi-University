667fb0d25f41311525624e6c1b47e9828c3630f8
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-07-07 10:37:06 +0530

    MU

---
+++ JavaScript/Lecture2.js
@@ -0,0 +1,19 @@
+function printSomething(){
+    console.log("Namaste JavaScript");
+    // document.write("Hello JS");
+}
+
+/**
+ * What is REPL and how we can use REPL?
+ * What is difference between == and === ?
+ * What is difference between null and undefined?
+ * Does JavaScipt support interpreter if yes, please explain how it's work?
+ * how many ways to declare function in JavaScript explain how we can declare?
+ */
+
+/**
+ * These are some youtube channel name where you can prepare JavaScript & Fronted Interview
+ * JavaScript Mastery
+ * Namaste JavaScript
+ * Code Evaluation
+ */
